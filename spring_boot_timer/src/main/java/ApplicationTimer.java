62841f8280a49b9019f8e8dd62323ad91e385715
Galy <piguanghua365@163.com>
2016-12-17 17:28:57 +0800

    spring boot 定时器最简单的demo

---
+++ spring_boot_timer/src/main/java/ApplicationTimer.java
@@ -0,0 +1,26 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by piguanghua on 2016/12/17.
+ */
+@Controller
+@EnableAutoConfiguration
+@ComponentScan({"component"})
+public class ApplicationTimer {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationTimer.class, args);
+    }
+}
