a513a790e5cef1a9433a595e5577439458e5fb14
Galy <piguanghua365@163.com>
2017-04-09 16:56:38 +0800

    使用Spring boot actuator模块

---
+++ spring_boot_actuator/src/main/java/com/acturtor/study/ActuatorApplication.java
@@ -0,0 +1,14 @@
+package com.acturtor.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zhenglian on 2016/10/7.
+ */
+@SpringBootApplication
+public class ActuatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorApplication.class, args);
+    }
+}
