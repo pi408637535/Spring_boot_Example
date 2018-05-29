da2d6aae671650c21840dfc70e63d5f95574d900
galy <piguanghua@wordemotion.com>
2018-05-29 11:23:20 +0800

    Spring book kafka测试

---
+++ Spring_boot_kafka/src/main/java/sample/kafka/SampleKafkaApplication.java
@@ -0,0 +1,16 @@
+package sample.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by piguanghua on 5/29/18.
+ */
+@SpringBootApplication
+public class SampleKafkaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SampleKafkaApplication.class, args);
+    }
+
+
+}
